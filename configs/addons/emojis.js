0f55869ab26578adcdcf2f0d2aa673e3c50f6cc1
Legend Umair ★ <arikaproxd.0@gmail.com>
2021-12-27 15:44:01 +0500

    Update emojis.js

--- configs/addons/emojis.js
+++ configs/addons/emojis.js
@@ -2,6 +2,6 @@ module.exports = {
     error: "<:error:924533606834118677>",
     success: '<:DynoSuccess:924548242866327602>',
     coin: '<a:Coin:924551520412897290>',
-    comet: '<:cometcircle:924877308194594856>',
+    comet: '<:comet:924975619333292053>',
     plus: '<:plus:924877552139526154>'
 };
